8bfa74cc6edf53b23df05e6dd730cf64021b8b2d
Samuel Pattuzzi <sam@pattuzzi.it>
2017-08-01 17:28:11 +0100

    Connecting Two Players

---
+++ Instructions.md
@@ -0,0 +1,15 @@
+## Connecting Two Players
+
+1. Create a 3rd person game
+
+ ![](Images/2017-08-01_17-12-26.png)
+
+2. Play as two (or more) Players
+
+ ![](Images/2017-08-01_17-20-15.png)
+
+3. Add more player starts
+
+ ![](Images/2017-08-01_17-23-10.png)
+
+4. Need to add many to avoid clash probability
---
+++ Puzzler/Source/Puzzler/PuzzlerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzlerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APuzzlerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APuzzlerGameMode();
+};
+
+
+
